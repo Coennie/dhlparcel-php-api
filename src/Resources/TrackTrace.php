cc79cccef5b807b474494f27ccb9e425944886ce
Coen van der Kolk <coen@nubos.nl>
2021-11-29 16:55:32 +0100

    Add status to tt

--- src/Resources/TrackTrace.php
+++ src/Resources/TrackTrace.php
@@ -10,10 +10,21 @@ class TrackTrace extends BaseResource
     /** @var bool */
     public $isDelivered;
 
+    /** @var string */
+    public $status;
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
         $this->isDelivered = collect($attributes)->has('deliveredAt');
+
+        $phases = ['DATA_RECEIVED', 'UNDERWAY', 'IN_DELIVERY', 'DELIVERED'];
+        foreach ($phases as $phase) {
+            $key = array_search($phase, array_column($attributes['view']->phaseDisplay, 'phase'));
+            if ($key !== false && $attributes['view']->phaseDisplay[$key]['completed'] === true) {
+                $this->status = $phase;
+            }
+        }
     }
 }
